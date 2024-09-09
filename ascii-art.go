b22ce02425e45446629ef5eb6d2de6bb60a9e05e
unknown <you@example.com>
2024-09-09 16:15:48 +0200

    ascii-art final version

--- ascii-art.go
+++ ascii-art.go
@@ -38,7 +38,7 @@ func readFile(chemin string) []string {
 func getRunesIndex(runes []rune) []int {
 	var indices []int
 	for _, r := range runes {
-		index := (int(r) - 32) * 9
+		index := ((int(r) - 32) * 9) + 2
 		indices = append(indices, index)
 	}
 	return indices
@@ -46,14 +46,13 @@ func getRunesIndex(runes []rune) []int {
 
 // Fonction récursive pour afficher les lignes intercalées pour chaque rune
 func recursiveGetLines(indices []int, lignes []string, ligneActuelle int) {
-	if ligneActuelle >= 9 {
-		return // Cas de base : arrêt après 9 lignes
+	if ligneActuelle >= 8 {
+		return //Cas de base : arrêt après 9 lignes
 	}
-
 	// Affichage de la ligneActuelle pour chaque rune
 	for _, index := range indices {
 		if index+ligneActuelle < len(lignes) {
-			fmt.Printf("%s ", lignes[index+ligneActuelle])
+			fmt.Printf("%s ", lignes[index+(ligneActuelle-1)])
 		}
 	}
 	fmt.Println()
@@ -68,38 +67,24 @@ func main() {
 		fmt.Println("Veuillez fournir une chaîne de caractères en argument.")
 		os.Exit(1)
 	}
-
-	// Récupérer la chaîne de caractères passée en argument
 	chaine := os.Args[1]
-
-	// Remplacer les occurrences de \n par des sauts de ligne réels
 	chaine = strings.ReplaceAll(chaine, `\n`, "\n")
-
-	// Diviser la chaîne en lignes
 	segments := strings.Split(chaine, "\n")
-
 	// Lire le fichier "standard.txt" et récupérer les lignes
 	lignes := readFile("standard.txt")
 
 	// Afficher chaque segment avec un espacement approprié
-	for i, segment := range segments {
-		// Si le segment est vide, cela correspond à un saut de ligne \n
+	for _, segment := range segments {
 		if segment == "" {
-			if i < len(segments)-1 {
-				fmt.Println() // Imprimer un seul saut de ligne pour le \n
-			}
-			continue
+			fmt.Print("\n") // Imprimer un seul saut de ligne pour le \n
+		} else {
+			runes := convertRunes(segment)
+
+			// Calculer les indices des runes dans le fichier
+			indices := getRunesIndex(runes)
+
+			// Afficher les lignes pour chaque rune de manière récursive
+			recursiveGetLines(indices, lignes, 0)
 		}
-
-		// Convertir la chaîne en runes
-		runes := convertRunes(segment)
-
-		// Calculer les indices des runes dans le fichier
-		indices := getRunesIndex(runes)
-
-		// Afficher les lignes pour chaque rune de manière récursive
-		recursiveGetLines(indices, lignes, 0)
-
-		// Supprimer l'appel à fmt.Println() ici pour éviter l'impression de plusieurs lignes vides
 	}
 }
